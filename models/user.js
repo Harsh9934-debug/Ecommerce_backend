65dd4396c2091eec6c1de1a3de25ad948ab171cd
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-01-31 00:14:21 +0530

    Updated the changes

--- models/user.js
+++ models/user.js
@@ -15,29 +15,27 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        minLength: 6
+        minLength: 5
     },
     role: {
         type: String,
         enum: ["customer", "admin"],
         default: "customer"
     }
-}, 
+},
     { timestamps: true }
 );
 
 
 // password hashed middleware
-userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 
 // password match middleware after the hashed
-
 userSchema.methods.matchPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
--- routes/userRouters.js
+++ routes/userRouters.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const User=require("../models/user");
 const jwt = require("jsonwebtoken");
-
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
@@ -9,7 +9,19 @@ router.post("/register",async(req,res)=>{
     const {name,email,password}=req.body;
 
     try {
-        res.send({name,email,password})
+        let user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({message:"User already exists"});
+        }
+        user = new User({name,email,password});
+        await user.save();
+
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role
+        });
     } catch (error) {
         console.log(error);
         res.status(500).json({message:"Internal Server Error"});
